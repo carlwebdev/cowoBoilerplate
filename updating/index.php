2f8ae65eb9a47726d1cc5afc61042085d1f9fb75
Carl Johansson <carlinspain@gmail.com>
2024-10-08 14:20:58 +0200

    commit updating folder

---
+++ updating/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'>
+<meta charset="UTF-8">
+<title>Carl Johansson - Front-End Web Developer</title>
+<meta name="description" content="Front-End Web Developer in Barcelona"> 
+<meta name="keywords" content="front-end web developer barcelona, web developer barcelona, websites barcelona, responsive web designer"> 	
+<link rel="shortcut icon" href="assets/favicon/favicon.ico" />
+<link rel="stylesheet" type="text/css" href="assets/css/global.css" >
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=yes" />
+</head>
+<body>
+<div id="page">
+    
+  <div class="ghostCenter"> 
+    <div class="section_date"> 
+      Carl Johansson<?php /*?><br>
+      .. spring cleaning time ...<?php */?>
+    </div> <!-- /section_date -->
+    <div class="section_details"> 
+      <a href="mailto:mail@carljohansson.net">mail@carljohansson.net</a><br>
+    </div>  
+  </div> <!-- /ghostCenter --> 
+  
+  <div class="l_bottom">  
+		<div class="m_developer"> 
+      <div class="text"><a href="http://cowowo.cat/" target="_blank">Powered by</a></div>
+      <div class="logo"> 
+        <a href="http://cowowo.cat/" target="_blank"><img src="assets/img/logos/logo_cowowo.png" alt="COWOWO"></a>
+      </div>    
+    </div> <!-- /m_developer -->
+  </div> <!-- /l_bottom --> 
+  
+</div> <!-- /page -->
+</body>
+</html>
